9a9986ab27b12b8b0409f13fdfcea104443ef5ad
Ryan <gamingryan235@gmail.com>
2019-05-31 14:15:31 +0900

    Complete roulette

--- js/roulette.js
+++ js/roulette.js
@@ -32,12 +32,12 @@ function getStar() {
  * Parse given probabilities from HTML.
  */
 function parseGivens() {
-    let givens = {}
+    let givens = [];
     $('.roulette__row').each((i, element) => {
-        let item = $(element).find('.roulette__item').text();
+        // let item = $(element).find('.roulette__item').val();
         let given = parseFloat($(element).find('.roulette__given_prob').val());
 
-        givens[item] = given / 100;  // Perecent to probability
+        givens.push(given / 100);  // Perecent to probability
     });
 
     return givens;
@@ -48,24 +48,24 @@ function parseGivens() {
  * @param {*} givens - Probabilities inputed by user.
  */
 function givensToActuals(givens) {
-    let actuals = {};
+    let actuals = [];
     let actualMissProbability = (
         // First paren: P(Star = n)
         // Second paren: P(Miss | Star = n)
         // TODO Magic numbers
         0
-        + (givens['꽝'] * 1  ) * (0.3)                      // 1 Star
-        + (givens['꽝'] * 0.8) * (0.7 * 0.8)                // 2 Stars
-        + (givens['꽝'] * 0.5) * (0.7 * 0.2 * 0.93)         // 3 Stars
-        + (givens['꽝'] * 0.3) * (0.7 * 0.2 * 0.07 * 0.97)  // 4 Stars
-        + (givens['꽝'] * 0.1) * (0.7 * 0.2 * 0.07 * 0.03)  // 5 Stars
+        + (givens[0] * 1  ) * (0.3)                      // 1 Star
+        + (givens[0] * 0.8) * (0.7 * 0.8)                // 2 Stars
+        + (givens[0] * 0.5) * (0.7 * 0.2 * 0.93)         // 3 Stars
+        + (givens[0] * 0.3) * (0.7 * 0.2 * 0.07 * 0.97)  // 4 Stars
+        + (givens[0] * 0.1) * (0.7 * 0.2 * 0.07 * 0.03)  // 5 Stars
     );
-    actuals['꽝'] = actualMissProbability;
+    actuals.push(actualMissProbability);
 
-    let extraProbabilityFactor = (givens['꽝'] - actualMissProbability) / (1 - givens['꽝']);
-    for (let key in givens) {
-        if (key == '꽝') { continue; }
-        actuals[key] = givens[key] * (1 + extraProbabilityFactor);
+    let extraProbabilityFactor = (givens[0] - actualMissProbability) / (1 - givens[0]);
+    for (let i in givens) {
+        if (i == 0) { continue; } // 꾕
+        actuals.push(givens[i] * (1 + extraProbabilityFactor));
     }
     
     return actuals;
@@ -83,32 +83,54 @@ function update() {
  */
 function updateActuals(actuals) {
     $('.roulette__row').each((i, element) => {
-        let item = $(element).find('.roulette__item').text();
-        $(element).find('.roulette__actual_prob').text(actuals[item] * 100);
+        // let item = $(element).find('.roulette__item').val();
+        $(element).find('.roulette__actual_prob').text(actuals[i] * 100);
     });
 }
 
 function updateGivenSumText(givens) {
     let total = 0
-    for (let key in givens) {
-        total += givens[key];
+    for (let i in givens) {
+        total += givens[i];
     }
     total *= 100;
+    total = +total.toFixed(4);
     $('.given_total').text(total.toString());
-    if(total != 100) {
-        $('.given_total').css('color', 'red');
-        $('.roulette__actual_prob').css('color', 'red');
-    } else {
+    if(100 - 0.0001 < total && total < 100 + 0.0001) {
         $('.given_total').css('color', 'black');
         $('.roulette__actual_prob').css('color', 'black');
+    } else {
+        $('.given_total').css('color', 'red');
+        $('.roulette__actual_prob').css('color', 'red');
     }
 }
 
 
-$( document ).ready(() => {
+function addRow() {
+    $("table").append(
+        "<tr class=\"roulette__row\">" +
+            "<td><input class=\"roulette__item\" value=\"상품 X\"></input></td>" +
+            "<td><input class=\"roulette__given_prob\" placeholder=\"%\" value=\"0\"></input></td>" +
+            "<td><span class=\"roulette__actual_prob\"></span>%</td>" +
+        "</tr>"
+    );
+}
+
+
+$(document).ready(() => {
     update();
     $('input.roulette__given_prob').change(function() {
         update();
     });
-
+    $('.roulette__actions__more_button').click(() => {
+        addRow();
+        if ($('.roulette__row').length >= 20) {
+            $('.roulette__warning__max_20').show();
+            $('.roulette__actions__more_button').hide();
+        }
+        $('input.roulette__given_prob').last().change(function() {
+            update();
+        });
+        update();
+    });
 });
